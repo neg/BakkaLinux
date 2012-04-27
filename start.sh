00e50e115193d5180019a9888e44b6d13758dda3
Erik Hugne <erik.hugne@ericsson.com>
2012-04-27 14:38:33 +0200

    added proper network startup/teardown scripts

--- start.sh
+++ start.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+macstr=("`printf '00:AD:BE:EF:%02X:%02X\n' $((RANDOM%256)) $((RANDOM%256))`")
+LOCATION="."
+KERNEL="vmlinuz"
+DISK="rootfs.ext2"
+				
+qemu-system-x86_64 -kernel $LOCATION/$KERNEL \
+-boot c \
+-m 512 \
+-append "root=/dev/sda rw panic=1 console=ttyS0, 115200" \
+-localtime \
+-no-reboot \
+-name Megatron \
+-net nic,vlan=0,model=e1000,macaddr=$macstr \
+-net tap,script=./qemu-ifup,downscript=./qemu-ifdown \
+-nographic \
+-hda $LOCATION/$DISK
 
-# console - setup seriel console
-# memmap  - reserve 20M of memore 50M in to be used by pramfs
+#-net user,net=192.168.2.0/24 \
 
-qemu-kvm \
-    -nographic \
-    -m 100 \
-    -append 'console=ttyS0,115200' \
-    -initrd initramfs.cpio.gz \
-    -kernel vmlinuz
